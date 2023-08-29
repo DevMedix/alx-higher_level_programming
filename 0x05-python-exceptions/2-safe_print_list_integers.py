65079c33a134b72f7a63d2c67f18366f6dd94473
DevMedix <rawlingsmichael01@gmail.com>
2023-08-29 01:12:31 +0100

    created safe_print_list_integers function

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+        length = 0
+        for item in my_list:
+            length += 1
+
+        for item in my_list[:x]:
+            if isinstance(item, int):
+                if x <= length:
+                    print(item, end='')
+                    count += 1
+                elif x > length:
+                    raise IndexError("list index out of range")
+        print()
+        return count
+    except TypeError:
+        return 0
