1cfc441f562686e958ba4f3aeb853938a4e5dc21
DevMedix <rawlingsmichael01@gmail.com>
2023-09-13 02:28:02 +0100

    created read_file module

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""This module contains a function to read text files"""
+
+
+def read_file(filename=""):
+    """ Reads a file and prints to stdout
+
+    Args:
+        filename (str): file to read from
+    """
+    with open(filename, encoding="utf-8") as file:
+        read_data = file.read()
+        print(read_data)
