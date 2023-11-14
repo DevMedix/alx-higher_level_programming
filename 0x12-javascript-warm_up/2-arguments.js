4df9e77db0ab4368d5020e7be8d0230bb4d87306
DevMedix <rawlingsmichael01@gmail.com>
2023-11-14 09:56:16 +0000

    modified 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const args = process.argv;
 
-if (args < 3) {
+if (args.length < 3) {
   console.log('No argument');
-} else if (args === 3) {
+} else if (args.length === 3) {
   console.log('Argument found');
-} else if (args > 3) {
+} else if (args.length > 3) {
   console.log('Arguments found');
 }
