b24652413225ec079ef5b1c420ec9533fafc17a9
DevMedix <rawlingsmichael01@gmail.com>
2023-11-14 13:46:37 +0000

    modified file

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args[2] == null) {
+if (args[2] == null || isNaN(args[2])) {
   console.log('Missing size');
 } else {
   for (let row = 0; row < parseInt(args[2]); row++) {
